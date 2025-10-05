08114bdb7a2a7f5da7eaeb8f9e769aafa8087752
shun-front <ogasawarashun49@gmail.com>
2025-10-05 17:14:21 +0900

    メッセージ定義ファイル作成

--- public/messages.php
+++ public/messages.php
@@ -2,3 +2,6 @@
 define('MSG_REQUIRED', '必須項目です');
 define('MSG_EMAIL', 'Emailの形式で入力してください');
 define('MSG_PASS_MINLEN', 'パスワードは6文字以上で入力してください');
+define('MSG_PASS_MAXLEN', 'パスワードは20文字以内で入力してください');
+define('MSG_PASS_PATTERN', 'パスワードには半角英数字および記号を使用してください');
+define('MSG_EMAIL_DUPLICATE', 'そのメールアドレスはすでに登録されています');
