11b999dd9425b7cef848b7d9eb849ebe0789f11a
shun-front <ogasawarashun49@gmail.com>
2025-10-05 17:13:31 +0900

    ユーザー登録機能完成

--- public/register.php
+++ public/register.php
@@ -1,20 +1,9 @@
 <?php
 require_once('functions.php');
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+require_once('db.php');
+include 'partials/header.php';
 
-//DB接続情報
-$host = 'db';
-$db = 'practice';
-$user = 'user';
-$pass = 'password';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-];
+$title = "ユーザー登録";
 
 $errors = [];
 
@@ -26,18 +15,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $errors['email'] = $msg;
     } elseif ($msg = validEmail($email)) {
         $errors['email'] = $msg;
+    } else {
+        if ($msg = validEmailDuplicate($email, $pdo)) {
+            $errors['email'] = $msg;
+        }
     }
 
-    if ($msg = validRequired($password)) {
-        $errors['password'] = $msg;
-    } elseif ($msg = validMinLen($password)) {
+    if ($msg = validPassword($password)) {
         $errors['password'] = $msg;
     }
 
 
     if (empty($errors)) {
         try {
-            $pdo = new PDO($dsn, $user, $pass, $options);
 
             $hash = password_hash($password, PASSWORD_DEFAULT);
 
@@ -53,42 +43,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+<section class="p-register">
+    <h1 class="p-register__title">ユーザー登録</h1>
+    <div class="p-register__formField">
+        <form action="register.php" method="POST" class="p-register__form">
+            <div class="p-register__inputField">
+                <div class="p-register__labelField">
+                    <label for="" class="p-register__label">メールアドレス</label>
+                    <span class="c-require">必須</span>
+                </div>
+                <?php if (!empty($errors['email'])): ?>
+                    <p class="c-error"><?php echo $errors['email']; ?></p>
+                <?php endif; ?>
+                <input type="email" name="email" class="c-input"
+                    value="<?php echo escapeHtml($_POST['email'] ??  ''); ?>">
+            </div>
 
-<!DOCTYPE html>
-<html lang="ja">
+            <div class="p-register__inputField">
+                <div class="p-register__labelField">
+                    <label for="" class="p-register__label">パスワード</label>
+                    <span class="c-require">必須</span>
+                </div>
+                <?php if (!empty($errors['password'])): ?>
+                    <p class="c-error"><?php echo $errors['password']; ?></p>
+                <?php endif; ?>
+                <input type="password" name="password" class="c-input">
+            </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ユーザー登録</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
+            <div class="p-register__btnContainer">
+                <button type="submit" class="c-btn p-register__btn">登録</button>
+            </div>
+        </form>
+    </div>
+</section>
+<a href="login.php">ログイン</a>
 
-<body>
-    <h1>ユーザー登録</h1>
-    <a href="login.php">ログイン</a>
-    <form action="register.php" method="POST" class="p-register__form">
-        <div class="p-register__inputContainer">
-            <label for="" class="p-register__label">メールアドレス</label>
-            <?php if (!empty($errors['email'])): ?>
-                <p class="c-error"><?php echo $errors['email']; ?></p>
-            <?php endif; ?>
-            <input type="email" name="email" class="p-register__input"
-                value="<?php echo escapeHtml($_POST['email'] ??  ''); ?>">
-        </div>
-
-        <div class="p-register__inputContainer">
-            <label for="" class="p-register__label">パスワード</label>
-            <?php if (!empty($errors['password'])): ?>
-                <p class="c-error"><?php echo $errors['password']; ?></p>
-            <?php endif; ?>
-            <input type="password" name="password" class="p-register__input">
-        </div>
-
-        <div class="p-register__btnContainer">
-            <button type="submit" class="c-btn">登録</button>
-        </div>
-    </form>
-</body>
-
-</html>
+<?php include 'partials/footer.php'; ?>
