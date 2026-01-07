92a890242a35cc239d06e85a49e6d32329f037b6
shun-front <ogasawarashun49@gmail.com>
2026-01-07 19:20:32 +0900

    returnの学習

--- public/js/index.js
+++ public/js/index.js
@@ -1,57 +1,49 @@
-// 欲望の後付け
-const wantMoney = (num) => {
-  return `${num}兆円ほしい`;
+const testReturn = () => {
+  return "ここで終了";
+
+  console.log("これは表示される？");
 };
 
-const res = wantMoney(10);
+const res = testReturn();
+
 console.log(res);
 
-// 条件分岐
-const wantMoney2 = (num) => {
-  if (num < 100) {
-    return "現実的ですね";
-  }
-  if (100 <= num && num < 1000) {
-    return "まあまあ欲張りです";
-  }
-
-  if (num >= 1000) {
-    return "欲望がインフレしています";
-  }
-};
-
-console.log(wantMoney2(99));
-console.log(wantMoney2(999));
-console.log(wantMoney2(10000));
-
-const checkType = (num) => {
-  if (typeof num !== "number") {
+const checkType = (value) => {
+  if (typeof value !== "number") {
     return "数値を入力してください";
-  }
-  if (typeof num === "number") {
-    return `${num}兆円あれば人生が変わる`;
+  } else {
+    return `${value}は正常な数値です`;
   }
 };
 
-console.log(checkType("a"));
-console.log(checkType(7));
+console.log(checkType("あ"));
+console.log(checkType(10));
 
-const profile = (name, job, age) => {
-  return `私の名前は${name}です。職業は${job}です。年齢は${age}です。`;
+const noReturn = () => {
+  const a = 10;
+  const b = 20;
+  const c = a + b;
 };
 
-const res4 = profile("田中", "エンジニア", 42);
+const res6 = noReturn();
+console.log(res6);
 
-console.log(res4);
+const validate = (value) => {
+  if (value === null) {
+    return "値がありません";
+  }
+  if (typeof value !== "number") {
+    return "数値を入力してください";
+  } else {
+    return "OK";
+  }
+};
+console.log(validate(null));
+// "値がありません"
 
-const introduce = ({name, age, job})=>{
-    return `私の名前は${name}です。職業は${job}です。年齢は${age}です。`;
-}
+console.log(validate("10"));
+// "数値を入力してください"
 
-const res5 = introduce({
-  age: 42,
-  job: "看護師",
-  name: "佐々木",
-});
+console.log(validate(10));
+// "OK"
 
-console.log(res5);
