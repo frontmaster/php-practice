5b33efdc27bd82d243b59e77f88e6a3b6d8f7a12
shun-front <ogasawarashun49@gmail.com>
2026-01-13 19:19:43 +0900

    オブジェクトに関数を入れてテスト

--- public/js/test.js
+++ public/js/test.js
@@ -1,30 +1,22 @@
 const { getType } = require("typechecker");
 
-describe("オブジェクトの中に配列を入れてテスト", () => {
-  const teams = ["tigers", "carp", "swallows"];
+describe("オブジェクトに関数を入れた場合のテスト", () => {
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
 
   const obj = {
-    title: "プロ野球チーム",
-    count: 12,
-    list: teams,
+    name: "加算の関数",
+    subject: "算数",
+    myFunc: add,
   };
-  it("オブジェクト全体の型チェック", () => {
-    const res = getType(obj);
-    expect(res).toEqual("object");
+
+  it("関数の型チェック", () => {
+    const res = getType(obj.myFunc);
+    expect(res).toEqual("function");
   });
-  it("オブジェクト内の配列の型と値をチェック", () => {
-    const res = getType(obj.list);
-    expect(res).toEqual("array");
-    expect(res).not.toEqual("object");
-    const res2 = obj.list;
-    expect(res2).toEqual(["tigers", "carp", "swallows"]);
-  });
-  it("titleの型チェック", () => {
-    const res = getType(obj.title);
-    expect(res).toEqual("string");
-  });
-  it("countの値チェック", () => {
-    const res = obj.count;
-    expect(res).toEqual(12);
+  it("関数の結果をチェック", () => {
+    const res = obj.myFunc(15, 24);
+    expect(res).toEqual(39);
   });
 });
