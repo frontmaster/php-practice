5180e92276da00e69fb14aa265c92a36fc993ae0
shun-front <ogasawarashun49@gmail.com>
2026-01-17 21:55:59 +0900

    総合問題

--- public/js/test.js
+++ public/js/test.js
@@ -2,88 +2,98 @@ const { getType } = require("typechecker");
 
 describe("問題1", () => {
   const judgeScore = (score) => {
-    if (score >= 80) {
+    if (score >= 90) {
+      return "S";
+    }
+    if (score >= 70) {
       return "A";
     }
-    if (score >= 60 && score < 80) {
+    if (score >= 50) {
       return "B";
     }
     return "C";
   };
-  it("80点以上はA", () => {
-    expect(judgeScore(80)).toEqual("A");
-  });
-  it("60点以上80点未満はB", () => {
-    expect(judgeScore(60)).toEqual("B");
-  });
-  it("60点未満はC", () => {
-    expect(judgeScore(59)).toEqual("C");
+  it("score判定", () => {
+    expect(judgeScore(95)).toEqual("S");
+    expect(judgeScore(70)).toEqual("A");
+    expect(judgeScore(50)).toEqual("B");
+    expect(judgeScore(40)).toEqual("C");
   });
 });
 
 describe("問題2", () => {
-  const prices = [100, 200, 300];
-  const res = prices.map((price) => {
-    return Math.round(price * 1.1);
+  const users = [
+    { name: "ken", age: 18 },
+    { name: "tanaka", age: 25 },
+  ];
+  const addAge = users.map((user) => {
+    return { name: user.name, age: user.age + 5 };
   });
-  it("税込価格に変換", () => {
-    expect(res).toEqual([110, 220, 330]);
+  it("addAgeのチェック", () => {
+    expect(addAge).toEqual([
+      { name: "ken", age: 23 },
+      { name: "tanaka", age: 30 },
+    ]);
   });
 });
 
 describe("問題3", () => {
-  const names = ["ken", "tanaka", "joe", "suzuki"];
-  const res = names.filter((name) => {
-    return name.length >= 5;
+  const names = ["ken", "tanaka", "joe", "sato", "oda"];
+  const selectName = names.filter((name) => {
+    return name.length >= 4 && name.includes("a");
   });
-  it("5文字以上の名前を抽出", () => {
-    expect(res).toEqual(["tanaka", "suzuki"]);
+  it("4文字以上かつaを含むname", () => {
+    expect(selectName).toEqual(["tanaka", "sato"]);
   });
 });
 
 describe("問題4", () => {
-  const utils = {
-    formatUser: (name) => {
-      return `User:${name}`;
-    },
-  };
-  const users = ["ken", "tanaka"];
-  const res = users.map((name) => {
-    return utils.formatUser(name);
-  });
-  it("Userを付ける", () => {
-    expect(res).toEqual(["User:ken", "User:tanaka"]);
-  });
-});
-
-describe("問題5", () => {
   const users = [
     { name: "ken", age: 18 },
     { name: "tanaka", age: 25 },
     { name: "sato", age: 30 },
   ];
-  const selectAge = users.filter((user) => {
+  const selectUser = users.filter((user) => {
     return user.age >= 20;
   });
-  const selectName = selectAge.map((user) => {
+  const resetUser = selectUser.map((user) => {
     return user.name;
   });
-  it("20歳以上の名前一覧", () => {
-    expect(selectName).toEqual(["tanaka", "sato"]);
+  it("20歳以上でnameのみ", () => {
+    expect(resetUser).toEqual(["tanaka", "sato"]);
+  });
+});
+
+describe("問題5", () => {
+  const names = ["ken", "tanaka"];
+  const utils = {
+    createName: (name) => {
+      return `Mr.${name}`;
+    },
+  };
+  const res = names.map((name) => {
+    return utils.createName(name);
+  });
+  it("Mr.をつけたname", () => {
+    expect(res).toEqual(["Mr.ken", "Mr.tanaka"]);
   });
 });
 
 describe("問題6", () => {
-  const obj = {
-    calc: (a, b) => {
-      return a + b;
-    },
-  };
-  it("calcは関数型", () => {
-    const res = getType(obj.calc);
-    expect(res).toEqual("function");
+  const users = [
+    { name: "ken", age: 18 },
+    { name: "tanaka", age: 25 },
+  ];
+  const addProperty = users.map((user) => {
+    if (user.age >= 20) {
+      return { name: user.name, age: user.age, isAdult: true };
+    }
+    return { name: user.name, age: user.age, isAdult: false };
   });
-  it("計算結果", () => {
-    expect(obj.calc(7, 8)).toEqual(15);
+  it("20歳以上かどうか", () => {
+    expect(addProperty).toEqual([
+      { name: "ken", age: 18, isAdult: false },
+      { name: "tanaka", age: 25, isAdult: true },
+    ]);
   });
 });
