491d5548abf3ed4154aa4221e59d2a0c272e039f
shun-front <ogasawarashun49@gmail.com>
2026-01-19 19:54:23 +0900

    総合問題

--- public/js/test.js
+++ public/js/test.js
@@ -1,31 +1,79 @@
 const { getType } = require("typechecker");
 
-describe("実務的ユーザー整形処理", () => {
+describe("総合問題", () => {
   const apiUsers = [
-    { id: 1, name: "ken", age: 18, skills: ["js"], deleted: false },
-    { id: 2, name: "tanaka", age: 25, skills: ["js", "vue"], deleted: false },
-    { id: 3, name: "sato", age: 30, skills: ["php"], deleted: true },
-    { id: 4, name: "oda", age: 17, skills: [], deleted: false },
+    {
+      id: 1,
+      name: "ken",
+      age: 18,
+      skills: ["js"],
+      loginCount: 5,
+      deleted: false,
+    },
+    {
+      id: 2,
+      name: "tanaka",
+      age: 25,
+      skills: ["js", "vue"],
+      loginCount: 40,
+      deleted: false,
+    },
+    {
+      id: 3,
+      name: "sato",
+      age: 35,
+      skills: ["php", "laravel", "docker"],
+      loginCount: 120,
+      deleted: false,
+    },
+    {
+      id: 4,
+      name: "oda",
+      age: 28,
+      skills: [],
+      loginCount: 2,
+      deleted: true,
+    },
   ];
 
-  const selectUser = apiUsers.filter((user) => {
+  const judgeUser = apiUsers.filter((user) => {
     return user.deleted === false && user.age >= 20;
   });
-  const newUser = selectUser.map((user) => {
-    return {
+  const selectUser = judgeUser.map((user) => {
+    const baseUser = {
       id: user.id,
-      displayName: "Mr." + user.name,
+      displayName: `Mr.${user.name}`,
       skillCount: user.skills.length,
       isAdult: user.age >= 20,
+      isActiveUser: user.loginCount >= 30,
     };
+    if (user.skills.length >= 3) {
+      return { ...baseUser, level: "senior" };
+    }
+    if (user.skills.length >= 1) {
+      return { ...baseUser, level: "middle" };
+    }
+    if (user.skills.length === 0) {
+      return { ...baseUser, level: "junior" };
+    }
   });
-  it("管理画面用ユーザー一覧を生成できる", () => {
-    expect(newUser).toEqual([
+  it("結果", () => {
+    expect(selectUser).toEqual([
       {
         id: 2,
         displayName: "Mr.tanaka",
         skillCount: 2,
         isAdult: true,
+        level: "middle",
+        isActiveUser: true,
+      },
+      {
+        id: 3,
+        displayName: "Mr.sato",
+        skillCount: 3,
+        isAdult: true,
+        level: "senior",
+        isActiveUser: true,
       },
     ]);
   });
