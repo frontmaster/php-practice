60851d2c341d2cdf7d397b62e6727d5f35a3f667
shun-front <ogasawarashun49@gmail.com>
2026-01-22 20:04:24 +0900

    分割代入

--- public/js/test.js
+++ public/js/test.js
@@ -1,46 +1,10 @@
-// const { getType } = require("typechecker");
-
-const apiResponse = [
-  {
-    id: 1,
-    name: "田中",
-    role: "member",
-    tasks: [
-      { id: 101, status: "todo" },
-      { id: 102, status: "done" },
-    ],
+const response = {
+  meta: {
+    page: 3,
+    perPage: 20,
   },
-  {
-    id: 2,
-    name: "佐藤",
-    role: "admin",
-    tasks: [],
-  },
-  {
-    id: 3,
-    name: "鈴木",
-    role: "member",
-    tasks: [{ id: 103, status: "todo" }],
-  },
-];
+};
 
-const selectMembers = apiResponse
-  .filter((response) => {
-    return response.role === "member";
-  })
-  .map((response) => {
-    const todoNumber = response.tasks.filter((task) => {
-      return task.status === "todo";
-    });
-    return {
-      id: response.id,
-      name: response.name,
-      todoCount: todoNumber.length,
-    };
-  });
+const { meta: { page: currentPage = 1 } = {} } = response;
 
-const judgeMember = selectMembers.filter((member) => {
-  return member.todoCount > 0;
-});
-
-console.log(judgeMember);
+console.log(currentPage);
