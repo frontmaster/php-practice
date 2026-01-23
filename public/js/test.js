51c92cf4baf8ed12b5aaeb054b2784219ad6812a
shun-front <ogasawarashun49@gmail.com>
2026-01-23 18:01:02 +0900

    sort

--- public/js/test.js
+++ public/js/test.js
@@ -1,10 +1,52 @@
-const response = {
-  meta: {
-    page: 3,
-    perPage: 20,
+const apiResponse = [
+  {
+    id: 1,
+    name: "ECサイト改修",
+    status: "active",
+    priority: "high",
+    startDate: "2025-02-01",
   },
-};
+  {
+    id: 2,
+    name: "社内ツール",
+    status: "pending",
+    priority: "low",
+    startDate: "2025-01-20",
+  },
+  {
+    id: 3,
+    name: "採用管理システム",
+    status: "active",
+    priority: "low",
+    startDate: "2025-01-25",
+  },
+  {
+    id: 4,
+    name: "予約システム",
+    status: "active",
+    priority: "high",
+    startDate: "2025-01-15",
+  },
+];
 
-const { meta: { page: currentPage = 1 } = {} } = response;
+const selectRes = apiResponse
+  .filter((res) => {
+    return res.status === "active";
+  })
+  .map((res) => {
+    return {
+      id: res.id,
+      name: res.name,
+      isHighPriority: res.priority === "high",
+      startDate: res.startDate,
+    };
+  });
 
-console.log(currentPage);
+const sortRes = selectRes.sort((a, b) => {
+  if (a.isHighPriority !== b.isHighPriority) {
+    return a.isHighPriority ? -1 : 1;
+  }
+  return new Date(a.startDate) - new Date(b.startDate);
+});
+
+console.log(sortRes);
