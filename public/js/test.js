49fbe4f4a05006d9591282c51f289835041c3369
shun-front <ogasawarashun49@gmail.com>
2026-01-24 19:37:45 +0900

    reduce

--- public/js/test.js
+++ public/js/test.js
@@ -1,36 +1,7 @@
-    const apiResponse = {
-    meta: {
-        page: 2,
-        perPage: 3,
-        total: 7,
-    },
-    data: [
-        { id: 4, title: "A", status: "open" },
-        { id: 5, title: "B", status: "closed" },
-        { id: 6, title: "C", status: "open" },
-    ],
-    };
-    const { page, perPage, total } = apiResponse.meta;
-    const lastPage = Math.ceil(total / perPage);
+const numbers = [10, 20, 30, 40];
 
-    const list = apiResponse.data
-    .filter((res) => {
-        return res.status === "open";
-    })
-    .map((res, index) => {
-        return {
-        no: (page - 1) * perPage + index + 1,
-        id: res.id,
-        title: res.title,
-        };
-    });
+const totalNum = numbers.reduce((acc, num) => {
+  return acc + num;
+}, 0);
 
-    const pagination = {
-    currentPage: page,
-    lastPage: lastPage,
-    hasPrevPage: page > 1,
-    hasNextPage: page < lastPage,
-    };
-
-    console.log(list);
-    console.log(pagination);
+console.log(totalNum);
