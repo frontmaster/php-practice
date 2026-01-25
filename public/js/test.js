a61d9f0ccd90a9fd7d0b2df1d330fc8b1b06d53b
shun-front <ogasawarashun49@gmail.com>
2026-01-25 19:25:55 +0900

    reduce

--- public/js/test.js
+++ public/js/test.js
@@ -1,7 +1,19 @@
-const numbers = [10, 20, 30, 40];
+const tasks = [
+  { id: 1, priority: "high" },
+  { id: 2, priority: "low" },
+  { id: 3, priority: "high" },
+  { id: 4, priority: "low" },
+  { id: 5, priority: "high" },
+];
 
-const totalNum = numbers.reduce((acc, num) => {
-  return acc + num;
-}, 0);
+const result = tasks.reduce((acc, task) => {
+  if (task.priority === "high") {
+    acc.high = (acc.high ?? 0) + 1;
+  }
+  if (task.priority === "low") {
+    acc.low = (acc.low ?? 0) + 1;
+  }
+  return acc;
+}, {});
 
-console.log(totalNum);
+console.log(result);
