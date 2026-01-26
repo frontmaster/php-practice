5b6c300738047e49e663e117f653879947a63687
shun-front <ogasawarashun49@gmail.com>
2026-01-26 22:10:56 +0900

    reduce

--- public/js/test.js
+++ public/js/test.js
@@ -1,12 +1,17 @@
-const nums = [1, 2, 2, 3, 3, 3, 4];
+const orders = [
+  { id: 1, userId: 10, amount: 1200, status: "completed" },
+  { id: 2, userId: 11, amount: 800, status: "pending" },
+  { id: 3, userId: 10, amount: 500, status: "completed" },
+  { id: 4, userId: 12, amount: 2000, status: "completed" },
+  { id: 5, userId: 11, amount: 700, status: "completed" },
+];
 
-const result = nums
-  .filter((num) => {
-    return num >= 2;
+const result = orders
+  .filter((order) => {
+    return order.status === "completed" && order.amount >= 1000;
   })
-  .reduce((acc, num) => {
-    acc[num] = (acc[num] ?? 0) + 1;
+  .reduce((acc, order) => {
+    acc[order.userId] = (acc[order.userId] ?? 0) + order.amount;
     return acc;
   }, {});
-
-  console.log(result);
+console.log(result);
