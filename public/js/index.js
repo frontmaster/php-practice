ea64f3706ef098d55935bfa568db90667cf5645a
shun-front <ogasawarashun49@gmail.com>
2026-02-08 14:37:03 +0900

    for、filter、map

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,48 @@
-const apiUsers = [
-  { id: 1, name: 'Taro', active: true, age: 25 },
-  { id: 2, name: 'Hanako', active: false, age: 17 },
-  { id: 3, name: 'Jiro', active: true, age: 32 },
+const users = [
+  { id: 1, age: 25, active: true },
+  { id: 2, age: 17, active: false },
+  { id: 3, age: 32, active: true },
 ];
 
-const userArr = [];
+const usersMap = users =>
+  users.filter(({ age, active }) =>
+    active && age >= 20).map(({ id }) =>
+      ({ id }));
 
-for (let i = 0; i < apiUsers.length; i++) {
-  userArr.push({ id: apiUsers[i].id, name: apiUsers[i].name, isAdult: (apiUsers[i].age >= 20) ? true : false, canLogin: (apiUsers[i].active && apiUsers[i].age >= 20) ? true : false });
+console.log(usersMap(users));
+
+
+const userArray = [];
+
+for (let i = 0; i < users.length; i++) {
+  if (users[i].active && users[i].age >= 20) {
+    userArray.push({ id: users[i].id });
+  }
 }
 
-console.log(userArr);
+console.log(userArray);
+
+const nums = [1, 2, 3, 4, 5];
+
+const numsArray = [];
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 2 === 0) {
+    numsArray.push(nums[i]);
+  }
+}
+
+console.log(numsArray);
+
+
+const product = { id: 1, price: 1000 };
+
+const newProduct = { ...product, price: 1200 }
+console.log(newProduct);
+
+// アロー関数に
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+const isEven = num => num % 2 === 0;
