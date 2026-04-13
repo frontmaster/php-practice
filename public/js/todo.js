7e1972db86a2c39cf329283b476ca29fa527e8b9
shun-front <ogasawarashun49@gmail.com>
2026-04-13 21:37:51 +0900

    完了ボタンと削除ボタンの処理を追加

--- public/js/todo.js
+++ public/js/todo.js
@@ -2,7 +2,7 @@ const onClickAdd = () => {
     // テキストボックスの値を取得し、初期化する
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value = "";
-    
+
     // li生成
     const li = document.createElement("li");
 
@@ -14,10 +14,29 @@ const onClickAdd = () => {
     const p = document.createElement("p");
     p.className = "todo-item";
     p.innerText = inputText;
-    
+
+    // button(完了)タグ生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.className = "c-btn btn-complete";
+    completeButton.addEventListener("click", () => {
+        alert("完了");
+    });
+
+    // button(削除)タグ生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    deleteButton.className = "c-btn btn-delete";
+    deleteButton.addEventListener("click", () => {
+        const deleteTarget = deleteButton.closest("li");
+        document.getElementById("incomplete-list").removeChild(deleteTarget);
+    });
+
     div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
     li.appendChild(div);
-    
+
     document.getElementById("incomplete-list").appendChild(li);
 }
 
