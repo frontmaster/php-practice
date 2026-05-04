c0d2ed0b88b8da5ec5011095c3bc78e26705a6b9
shun-front <ogasawarashun49@gmail.com>
2026-05-05 06:43:43 +0900

    編集機能追加

--- public/js/vue-main.js
+++ public/js/vue-main.js
@@ -6,6 +6,7 @@ createApp({
       newName: "",
       newAge: "",
       users: [],
+      editingUser: null,
     }
   },
 
@@ -23,6 +24,23 @@ createApp({
 
     deleteInfo(id) {
       this.users = this.users.filter(user => user.id !== id);
+    },
+
+    editInfo(user) {
+      this.editingUser = user;
+      this.newName = user.name;
+      this.newAge = user.age;
+    },
+
+    updateInfo(){
+      if(!this.newName.trim() || !this.newAge.trim()) return;
+
+      this.editingUser.name = this.newName;
+      this.editingUser.age = Number(this.newAge);
+
+      this.editingUser = null;
+      this.newName = "";
+      this.newAge = "";
     }
 
   }
